4a909b80fb3071a236a6601c3827899be6c3bf88
r2r-dev <dev@r2r.sh>
2021-08-09 02:59:27 +0200

    Temporarily revert `scan-nix` to previous implementation, which generates Bazel targets properly (see `nix_file_deps`)

--- nix/packages/scan_nix/src/main.rs
+++ nix/packages/scan_nix/src/main.rs
@@ -1,273 +1,198 @@
-use std::env;
-use std::path::{Path, PathBuf};
-
-use lorri::watch::WatchPathBuf;
-use lorri::AbsPathBuf;
+use lorri::{
+    builder, cas::ContentAddressable, nix::options::NixOptions, project::Project,
+    watch::WatchPathBuf, AbsPathBuf, NixFile,
+};
 use serde::Serialize;
+use std::env;
+use std::fs;
+use std::iter::FromIterator;
+use std::ops::Not;
+use std::path::Path;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 #[derive(Serialize)]
 struct DepSet {
-  kind: String,
-  files: Vec<String>,
+    kind: String,
+    files: Vec<String>,
 }
 
 #[derive(Serialize)]
 struct DepSets {
-  depsets: Vec<DepSet>,
-}
-
-#[derive(Debug, Clone)]
-pub enum ErrorKind {
-  AbsPathBufError,
-  IoError,
-  LorriError,
-  NoneError,
-  Other,
-  SerializationError,
-}
-
-#[derive(Debug, Clone)]
-pub struct ScanError {
-  pub kind: ErrorKind,
-  pub msg: String,
-}
-
-impl ScanError {
-  pub fn new(kind: ErrorKind, msg: String) -> Self {
-    Self { kind, msg }
-  }
-
-  pub fn new_from_str(kind: ErrorKind, msg: &str) -> Self {
-    Self {
-      kind,
-      msg: msg.to_string(),
-    }
-  }
-}
-
-impl std::fmt::Display for ScanError {
-  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    write!(f, "{}", self.msg)
-  }
-}
-
-impl From<std::io::Error> for ScanError {
-  fn from(error: std::io::Error) -> Self {
-    ScanError::new(ErrorKind::IoError, error.to_string())
-  }
-}
-
-impl From<lorri::error::BuildError> for ScanError {
-  fn from(error: lorri::error::BuildError) -> Self {
-    ScanError::new(ErrorKind::LorriError, error.to_string())
-  }
-}
-
-impl From<serde_json::Error> for ScanError {
-  fn from(error: serde_json::Error) -> Self {
-    ScanError::new(ErrorKind::SerializationError, error.to_string())
-  }
-}
-
-impl From<PathBuf> for ScanError {
-  fn from(pathbuf: PathBuf) -> Self {
-    ScanError::new(
-      ErrorKind::AbsPathBufError,
-      format!("Unable to create absolute path from: '{:?}'", pathbuf),
-    )
-  }
-}
-
-fn _prepare_tmpdir_root() -> Result<tempfile::TempDir, ScanError> {
-  tempfile::Builder::new()
-    .prefix("scan-nix")
-    .tempdir()
-    .map_or_else(|e| Err(ScanError::from(e)), |temp_dir| Ok(temp_dir))
-}
-
-fn _get_metadata(
-  tmp_dir: &tempfile::TempDir,
-  project_nix_file: &str,
-) -> Result<lorri::project::Project, ScanError> {
-  let nix_file_path = AbsPathBuf::new(PathBuf::from(project_nix_file))?;
-  let cas_tmp_dir_path = AbsPathBuf::new(tmp_dir.path().clone().join("caas"))?;
-  let gc_roots_tmp_dir_path =
-    AbsPathBuf::new(tmp_dir.path().clone().join("gc_roots"))?;
-
-  std::fs::create_dir(&cas_tmp_dir_path)?;
-  std::fs::create_dir(&gc_roots_tmp_dir_path)?;
-
-  let project = lorri::project::Project::new(
-    lorri::NixFile::from(nix_file_path),
-    &gc_roots_tmp_dir_path,
-    lorri::cas::ContentAddressable::new(cas_tmp_dir_path)?,
-  )?;
-  Ok(project)
-}
-
-fn _get_all_paths_without_nix_store(
-  project: &lorri::project::Project,
-) -> Result<Vec<WatchPathBuf>, ScanError> {
-  let build_result = lorri::builder::run(
-    &project.nix_file,
-    &project.cas,
-    &lorri::nix::options::NixOptions::empty(),
-  )?;
-  Ok(
-    build_result
-      .referenced_paths
-      .into_iter()
-      .filter(|path| !path.as_ref().starts_with("/nix/store"))
-      .collect::<Vec<WatchPathBuf>>(),
-  )
-}
-
-fn _get_nix_project_path(
-  project: &lorri::project::Project,
-) -> Result<AbsPathBuf, ScanError> {
-  match project.nix_file.as_absolute_path().to_path_buf().parent() {
-    Some(parent_path) => Ok(AbsPathBuf::new(parent_path.to_path_buf())?),
-    _ => Err(ScanError::new_from_str(
-      ErrorKind::NoneError,
-      "Value not found!",
-    )),
-  }
-}
-
-fn _is_child_of_nix_project(
-  project: &lorri::project::Project,
-  file_path: &AbsPathBuf,
-) -> bool {
-  match _get_nix_project_path(project) {
-    Ok(project_path) => file_path
-      .as_absolute_path()
-      .starts_with(project_path.as_absolute_path()),
-    _ => false,
-  }
-}
-
-fn _to_list_of_direct_bzl_deps(
-  project: &lorri::project::Project,
-  file_paths: Vec<AbsPathBuf>,
-) -> Result<Vec<String>, ScanError> {
-  let parsed_file = &env::var("NIX_FILE").unwrap().to_owned();
-  let nix_project_file = Path::new(parsed_file).to_path_buf();
-  let nix_project_path = nix_project_file.parent().unwrap();
-
-  Ok(
-    file_paths
-      .into_iter()
-      .filter_map(|path| {
-        match path.as_absolute_path().strip_prefix(nix_project_path) {
-          Ok(pth) => match pth.to_str() {
-            Some(p) => Some(String::from(p)),
-            None => None,
-          },
-          _ => None,
-        }
-      })
-      .collect::<Vec<String>>(),
-  )
-}
-
-fn _get_workspace_root_path() -> Result<AbsPathBuf, ScanError> {
-  // TODO: Fix this
-  let cwd = std::env::current_dir()?;
-  let path = AbsPathBuf::new(PathBuf::from(cwd))?;
-  Ok(path)
-}
-
-fn _to_list_of_bzl_deps(
-  file_paths: Vec<AbsPathBuf>,
-) -> Result<Vec<String>, ScanError> {
-  let workspace_root = _get_workspace_root_path()?;
-  let workspace_root_path = workspace_root.as_absolute_path();
-
-  Ok(
-    file_paths
-      .into_iter()
-      .filter_map(|path| {
-        match path.as_absolute_path().strip_prefix(workspace_root_path) {
-          Ok(pth) => match pth.to_str() {
-            Some(p) => Some(format!("//{}", String::from(p))),
-            None => None,
-          },
-          _ => None,
-        }
-      })
-      .collect::<Vec<String>>(),
-  )
-}
-
-fn _scan() -> Result<(), ScanError> {
-  let args: Vec<String> = std::env::args().collect::<Vec<String>>();
-  let project_file = args.get(1).ok_or(ScanError::new_from_str(
-    ErrorKind::NoneError,
-    "Value not found!",
-  ))?;
-  let tempdir = _prepare_tmpdir_root()?;
-
-  let project = _get_metadata(&tempdir, project_file)?;
-
-  let project_files = _get_all_paths_without_nix_store(&project)?
-    .into_iter()
-    .flat_map(|path| {
-      match path {
-        WatchPathBuf::Normal(pth) => vec![pth],
-        // If path is a directory, translate it to list of files contained
-        // within it
-        WatchPathBuf::Recursive(pth) => walkdir::WalkDir::new(pth)
-          .into_iter()
-          .filter_map(|result| match result {
-            Ok(entry) => {
-              if !entry.file_type().is_dir() {
-                Some(entry.into_path())
-              } else {
-                None
-              }
-            }
-            _ => None,
-          })
-          .collect::<Vec<PathBuf>>(),
-      }
-    })
-    .map(AbsPathBuf::new)
-    .filter_map(|result| match result {
-      Ok(pth) => Some(pth),
-      _ => None,
-    })
-    .collect::<Vec<AbsPathBuf>>();
-
-  let (project_children, mut project_deps): (Vec<AbsPathBuf>, Vec<AbsPathBuf>) =
-    project_files
-      .into_iter()
-      .partition(|file_path| _is_child_of_nix_project(&project, file_path));
-  // Every children is also a project dep
-  project_deps.extend(project_children.clone());
-
-  // TODO: Better naming
-  let all_deps = DepSets {
-    depsets: vec![
-      DepSet {
-        kind: "recursive".to_string(),
-        files: _to_list_of_bzl_deps(project_deps)?,
-      },
-      DepSet {
-        kind: "direct".to_string(),
-        files: _to_list_of_direct_bzl_deps(&project, project_children)?,
-      },
-    ],
-  };
-
-  let all_deps_repr = serde_json::to_string(&all_deps)?;
-  println!("{}", all_deps_repr);
-
-  Ok(())
+    depsets: Vec<DepSet>,
 }
 
 fn main() {
-  match _scan() {
-    Ok(_) => (),
-    Err(error) => println!("{:?}", error),
-  }
+    let args: Vec<String> = env::args().collect();
+    let tempdir = tempfile::tempdir().expect("tempfile::tempdir() failed us!");
+
+    let project_file = &args[1];
+    let project = project(
+        project_file,
+        &lorri::AbsPathBuf::new(tempdir.path().to_owned()).unwrap(),
+    );
+
+    // FIXME: compute strip prefix differently
+    let p = env::current_dir().unwrap().to_str().unwrap().to_string();
+
+    let output = builder::run(&project.nix_file, &project.cas, &NixOptions::empty())
+        .unwrap()
+        .referenced_paths;
+    let v = output
+        .into_iter()
+        .filter(|i| !i.as_ref().starts_with("/nix/store"))
+        .collect::<Vec<_>>();
+
+    let mut files: Vec<String> = Vec::new();
+    let mut depset_v: Vec<DepSet> = Vec::new();
+
+    for (_pos, e) in v.iter().enumerate() {
+        match e {
+            WatchPathBuf::Recursive(e) => walk(
+                &e.as_os_str().to_os_string().into_string().unwrap(),
+                &p,
+                &mut files,
+            ),
+            WatchPathBuf::Normal(e) => files.push(
+                strip(
+                    &e.as_os_str()
+                        .to_os_string()
+                        .into_string()
+                        .unwrap()
+                        .strip_prefix(&p)
+                        .unwrap(),
+                )
+                .to_owned(),
+            ),
+        };
+    }
+
+    files.sort_by_key(|k| k.len());
+    files.reverse();
+    let defaultNixes = &files
+        .to_owned()
+        .into_iter()
+        .filter(|i| i.ends_with("/default.nix"))
+        .collect::<Vec<_>>();
+    let bases: Vec<String> = defaultNixes
+        .iter()
+        .map(|x| x.strip_suffix("/default.nix").unwrap().to_string())
+        .collect();
+    let packages: Vec<String> = bases
+        .iter()
+        .map(|x| format!("{}:", x.to_string()))
+        .collect();
+
+    let mut targets: Vec<String> = Vec::new();
+    let mut visited: Vec<String> = Vec::new();
+    // FIXME
+    for i in 0..bases.len() {
+        for j in 0..files.len() {
+            if visited.contains(&files[j]).not() && files[j].starts_with(&bases[i]) {
+                let file_path = &files[j].to_owned();
+                let target_path = format!(
+                    "{}{}{}",
+                    "//".to_string(),
+                    packages[i],
+                    strip(&file_path.strip_prefix(&bases[i]).unwrap().to_string()).to_owned()
+                );
+                targets.push(target_path);
+                visited.push(file_path.to_string());
+            };
+        }
+    }
+
+    let recursive_depset = DepSet {
+        kind: "recursive".to_owned(),
+        files: targets,
+    };
+    depset_v.push(recursive_depset);
+
+    let parsed_file = &env::var("NIX_FILE").unwrap().to_owned();
+    let project_file_path = Path::new(parsed_file);
+    let project_dir = if project_file_path.is_dir() {
+        project_file_path
+    } else {
+        project_file_path.parent().unwrap()
+    };
+    let mut local_files: Vec<String> = Vec::new();
+    for (_pos, e) in v.iter().enumerate() {
+        match e {
+            WatchPathBuf::Recursive(e) => walk(
+                &e.as_os_str().to_os_string().into_string().unwrap(),
+                &p,
+                &mut local_files,
+            ),
+            WatchPathBuf::Normal(e) => local_files.push(
+                strip(
+                    &e.as_os_str()
+                        .to_os_string()
+                        .into_string()
+                        .unwrap()
+                        .strip_prefix(&p)
+                        .unwrap(),
+                )
+                .to_owned(),
+            ),
+        };
+    }
+    let project_dir_prefix = project_dir.to_str().unwrap().to_string();
+    let local_pref = strip(project_dir_prefix.strip_prefix(&p).unwrap());
+    local_files.retain(|i| i.starts_with(&local_pref));
+    let stripped_local_files: Vec<_> = local_files
+        .iter()
+        .map(|x| strip(x.strip_prefix(&local_pref).unwrap()).to_string())
+        .collect();
+    let direct_depset = DepSet {
+        kind: "direct".to_owned(),
+        files: stripped_local_files,
+    };
+
+    depset_v.push(direct_depset);
+
+    let depsets = DepSets { depsets: depset_v };
+
+    let l = serde_json::to_string(&depsets);
+
+    println!("{}", l.unwrap());
+}
+
+fn project(name: &str, cache_dir: &AbsPathBuf) -> Project {
+    let test_root = AbsPathBuf::new(PathBuf::from_iter(&[env!("CARGO_MANIFEST_DIR"), name]))
+        .expect("CARGO_MANIFEST_DIR was not absolute");
+    let cas_dir = cache_dir.join("cas").to_owned();
+    fs::create_dir_all(&cas_dir).expect("failed to create CAS directory");
+    Project::new(
+        NixFile::from(test_root),
+        &cache_dir.join("gc_roots"),
+        ContentAddressable::new(cas_dir).unwrap(),
+    )
+    .unwrap()
+}
+
+fn strip(s: &str) -> &str {
+    let mut chars = s.chars();
+    chars.next();
+    chars.as_str()
+}
+
+fn walk(s: &str, p: &str, files: &mut Vec<String>) {
+    for entry in WalkDir::new(s) {
+        let entry = entry.unwrap();
+        if entry.file_type().is_file() {
+            files.push(
+                strip(
+                    entry
+                        .path()
+                        .as_os_str()
+                        .to_os_string()
+                        .into_string()
+                        .unwrap()
+                        .strip_prefix(&p)
+                        .unwrap(),
+                )
+                .to_owned(),
+            );
+        };
+    }
 }
