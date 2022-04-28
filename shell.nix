c060ddfe0ea254090f9cc370c79a41ae5561b3ce
Aleksander Gondek <gondekaleksander@protonmail.com>
2022-04-28 13:05:55 +0200

    Introduce VSConfig to work on golang code

--- shell.nix
+++ shell.nix
@@ -3,5 +3,15 @@
 with pkgs;
 
 mkShell {
-  buildInputs = [ bazel_4 binutils cacert nix openjdk11 python3 less ];
+  buildInputs = [ bazel_4 binutils cacert go go-tools nix openjdk11 python3 less ];
+  shellHook = ''
+    mkdir -p $(pwd)/.go
+    mkdir -p $(pwd)/.gocache
+    ln -fs ${pkgs.go}/share/go $(pwd)/.goroot
+    ln -fs ${pkgs.go-tools} $(pwd)/.gotools
+
+    export GO11MODULE=on
+    export GOCACHE=$(pwd).gocache
+    export GOPATH=$(pwd)/.go
+  '';
 }
